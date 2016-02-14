3607903c9a1220df6eec75d7289b247555e4812c
sc14tsk <sc14tsk@leeds.ac.uk>
2016-02-14 05:51:36 +0000

    included images

--- Server/Client.py
+++ Server/Client.py
@@ -1,16 +1,11 @@
 import socket
-import sys
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+HOST = ''    #server name goes in here
+PORT = 3820
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST,PORT))
+with open('myUpload.txt', 'rb') as file_to_send:
+    for data in file_to_send:
+        socket.sendall(data)
+print('end')
+socket.close()t
