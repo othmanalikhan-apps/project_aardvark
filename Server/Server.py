c7f35950303435791ecd44e7e65d81556288df0e
sc14omsa <sc14omsa@leeds.ac.uk>
2016-02-19 14:55:50 +0000

    add table class

--- Server/Server.py
+++ Server/Server.py
@@ -1,21 +1,16 @@
-import socketserver
+"""
+The client side of the restaurant server-client system
+"""
 
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
+import http.server
 
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
-        socket.sendto(data.upper(), self.client_address)
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
+def runServer():
+    serverAddress = ('127.0.0.1', 8000)
+    serverClass = http.server.HTTPServer
+    handlerClass = http.server.BaseHTTPRequestHandler
+    server = serverClass(serverAddress, handlerClass)
     server.serve_forever()
+
+
+runServer()
