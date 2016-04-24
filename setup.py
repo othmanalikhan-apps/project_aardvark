e1b36d3e133470af4cdbc10829699891495fe531
sc14omsa <sc14omsa@leeds.ac.uk>
2016-04-24 01:36:45 +0100

    fixed dependency issues on Django that crashed builds

--- setup.py
+++ setup.py
@@ -272,7 +272,7 @@ setup(
     version='1.0',
     packages=find_packages(),
 
-    install_requires=['requests', 'Sphinx', 'django'],
+    install_requires=['requests', 'Sphinx', 'Django'],
 
     cmdclass={
         'runInstall': InstallInVirtualEnv,
